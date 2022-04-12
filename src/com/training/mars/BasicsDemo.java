1904c45de90d4328bbf4abe30d70e24d7c87bac6
dhana <dhanak983@gmail.com>
2022-04-12 10:44:29 -0500

    Completed

--- src/com/training/mars/BasicsDemo.java
+++ src/com/training/mars/BasicsDemo.java
@@ -5,14 +5,16 @@ public class BasicsDemo {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-         int age = 21;
+		System.out.println("Enter Age: ");
+         int age = scanner.nextInt();
+         
          if(age<18){
         	 System.out.println("not eligible to vote");
          }
          else{
         	 System.out.println("Eligible to vote");
          }
-
+      scanner.close();
 	}
 
 }
